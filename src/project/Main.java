c14412050f7dec7839aa740d504cf975f090fd70
Anastasia <90572535+aurummiak@users.noreply.github.com>
2024-09-07 20:46:32 +0300

    Update Main.java

--- src/project/Main.java
+++ src/project/Main.java
@@ -10,17 +10,17 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         ArrayList<String> list = new ArrayList<>();
-        System.out.println("--Add--"); // 1
+        System.out.println("--Add--"); // add new
         add(list);
-        System.out.println("--Delete--"); // 1
+        System.out.println("--Delete--"); // delete
         delete(list);
-        System.out.println("--Find Substring--"); // 6
+        System.out.println("--Find Substring--"); // search
         search(list);
-        System.out.println("--Work with files--"); // 7
+        System.out.println("--Work with files--"); // init
         initialize(list);
-        System.out.println("--XML--"); // 3
+        System.out.println("--XML--"); // XML writing
         writeXmlFile(list);
-        System.out.println("--Compare 2 elements--"); // 8
+        System.out.println("--Compare 2 elements--"); // compare
         compareElements(list);
 
 
